19ff37217269d514b1c5dc44fe6dfa51e4a13fb8
deepak-8bit <deepakmahehwar515@gmail.com>
2022-02-17 15:32:31 +0530

    uploding files in closne

--- loggin/src/main/java/com/example/loggin/model/User.java
+++ loggin/src/main/java/com/example/loggin/model/User.java
@@ -20,6 +20,10 @@ public class User {
 	public void setName(String name) {
 		this.name = name;
 	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + "]";
+	}
 	
 	
 }
